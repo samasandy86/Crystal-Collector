7afc8f25a1b6de31e01fed66281d819adfa49a7b
Sama Sandy <samasandy86@gmail.com>
2018-03-12 23:41:32 -0500

    Adding final documents. Added sounds and images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,77 +1,130 @@
 'use strict'
+$(document).ready(function () {
 
+	// =============================
+	// Set up global variables first
+	// =============================
 
-/* ---- INFORMATION ABOUT THE GAME ---  */
+	// initialize global variables
+	let rangerOneValue, rangerTwoValue, rangerThreeValue, rangerFourValue, rangerFiveValue;
+	let wins = 0;
+	let losses = 0;
+	// randomNumber is the random digit computer will generage
+	let randomNumber;
+	// userScore is the ongoing sum of ranger values user has picked
+	let userScore;
 
-// A game with 4 crsytals and Random Result
-// Every cyrstal needs to have a random number between 1  - 50
-// A new random number should be generated every single time we Win or Lose to those crystals
-// Matching the target number is your GOAL.
-
-// When clicking any CRYSTAL, it should add with the previous result, until it equal the TOTAL SCORE
-// If it is GREATER than the RANDOM NUMBER, then we START OVER and DECREMENT the lost counter
-// Iif it DOES equal, then we increment a win counter
-
-
-
-//List of variables
-
-let counter = 0;    //Counter starts off with 0 but will change based on your points
-let winCount;       //Total number of Wins
-let lossCount;      //Total Number of Losses
-let targetNumber;   //Number that is randomly generated for the player to try
-
-
-/* LOOP */
-
-//This FOR LOOP generates 4 random numbers that are injected into our buttons/players.
-for (let i = 0; i < 4; i++) {
-    let random = Math.floor(Math.random() * 15) + 1; // this math random will randomly store numbers between
-                                                    // 1 and 15
-    
-    let button = $("<div>");
-        button.attr({
-            "class": "btn-group",
-            "data-random": random
-        });
-
-        $(".btn-group").append(button);
-   
-    console.log(random);             // This console log shows the random numbers being generated
-                                    //  is injecting it into  the BUTTON GROUP class (".btn-group")
-}
-
-
-/* --- RANDOM NUMBER CODE --- */
-
-// As a user when you open the game, you should see a randomly generated TARGET NUMBER. 
-// This is the fuction for the random number generator
-// Once the random number is generated I need to assign the random number to
-// variable to used later.
-
-function randomNumber() {
-    return Math.floor(Math.random() * 70) + 20;
-}; 
-    targetNumber = randomNumber();              // This assigns random number to the to the TARGET NUMBER variable.
-    $('#target-number').text(targetNumber);     // Now the Random number needs to displayed on the screen.  So we write a jQuery selector to display to the DOM.
-
-
-// As a user the game needs default settings; everything is set to 0.
-// This function will be used for reset, when you win, lose and default.
-// This will set the target number and counter to 0 and regenerate a number for your crystals.
-
-function reset() {
-    counter = 0;
-    targetNumber = randomNumber();
-    $('#counter-number').text(counter);
-
-};
+	// Sound EFX
+	let biteSound = new Audio('./assets/sounds/bite.wav');
+	let kickSound = new Audio('./assets/sounds/kick.wav');
+	let hitSound = new Audio('./assets/sounds/hit.wav');
+	let slapSound = new Audio('./assets/sounds/slap.wav');
+	let uppercutSound = new Audio('./assets/sounds/uppercut.wav');
+	let winSound = new Audio('./assets/sounds/win-rangers.mp3');
+	let loseSound = new Audio('./assets/sounds/you-lose.wav');
 
 
 
+	// ================
+	// Set up functions
+	// ================
+
+	// function that initializes the variables for each new round of the game
+	function setVariables() {
+		// have computer pick a number between 19-120
+		randomNumber = 19 + Math.floor(Math.random() * 102);
+		// pick random ranger values between 1-12
+		rangerOneValue = 1 + Math.floor(Math.random() * 12);
+		rangerTwoValue = 1 + Math.floor(Math.random() * 12);
+		rangerThreeValue = 1 + Math.floor(Math.random() * 12);
+		rangerFourValue = 1 + Math.floor(Math.random() * 12);
+		rangerFiveValue = 1 + Math.floor(Math.random() * 12);
+
+
+		// set initial value of user's ongoing ranger selections sum to 0
+		userScore = 0;
+		// update the html for the game board
+		$("#winsTally").html("Wins: " + wins);
+		$("#lossesTally").html("Losses: " + losses);
+		$("#randomNumber").html(randomNumber);
+		$("#userScore").html(userScore);
+		consoleLogVariables();
+	}
+
+
+
+	// function to check if user has won or lost
+	// increment wins / losses in either case
+	// and then re-initialize variables for new round
+	// if user hasn't won or lost then nothing happens
+
+	function gameStatus() {
+		// check if user has lost
+		if (userScore > randomNumber) {
+			losses++;
+			loseSound.play();
+			console.log("user lost");
+			setVariables();
+		}
+
+		// check if user has won
+		if (userScore == randomNumber) {
+			wins++;
+			winSound.play();
+			console.log("user won");
+			setVariables();
+		}
+	}
 
 
 
 
+	// debugging functionality function
+	function consoleLogVariables() {
+		console.log("wins: " + wins + " losses: " + losses);
+		console.log("rangerOneValue: ", rangerOneValue + " rangerTwoValue: " + rangerTwoValue + " rangerThreeValue: " + rangerThreeValue + " rangerFourValue: " + rangerFourValue + " rangerFiveValue: " + rangerFiveValue);
+		console.log("randomNumber: " + randomNumber + " userScore: " + userScore);
+		console.log("----------------------------------");
 
+	}
 
+	// =====================================
+	// Main Game Functionality
+	// =====================================
+
+	setVariables();
+
+	// listen for clicks on any of the rangers by targeting the ranger class
+	$(".ranger").on("click", function () {
+		// each ranger has a value attribute of ranger1 - ranger5
+		// use this attribute to identify which ranger the user actually clicked
+		var pressed = $(this).attr("value");
+		console.log(pressed);
+		// add the value of the ranger to the user's ongoing score tally
+		if (pressed == "ranger1") {
+			userScore += rangerOneValue;
+			biteSound .play();
+		} else if (pressed == "ranger2") {
+			userScore += rangerTwoValue;
+			kickSound.play();
+		} else if (pressed == "ranger3") {
+			userScore += rangerThreeValue;
+			kickSound.play();
+		} else if (pressed == "ranger4") {
+			userScore += rangerFourValue;
+			slapSound.play();
+		} else if (pressed == "ranger5") {
+			userScore += rangerFiveValue;
+			uppercutSound.play();
+		}
+		else {
+			console.log("error");
+		}
+		// then update the html for the user score
+		$("#userScore").html(userScore);
+		consoleLogVariables();
+		// call the function to see if user has won or lost
+		gameStatus();
+	});
+
+});
